6780b0ff92704ada26760762e1b347dcac608764
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-10-06 22:02:05 +0530

    import

---
+++ FarmingJava/src/com/xworkz/FarmingJava/boot/Papaya.java
@@ -0,0 +1,10 @@
+package com.xworkz.FarmingJava.boot;
+
+public class Papaya {
+	static void main2 ()	{
+		System.out.println("Varite Taiwan786");
+		System.out.println("Age 1 year");
+
+	}
+}
+
---
+++ FarmingJava/src/com/xworkz/FarmingJava/boot/Pomogranate.java
@@ -0,0 +1,10 @@
+package com.xworkz.FarmingJava.boot;
+
+public class Pomogranate {
+static void main1 () {	
+	System.out.println("Hi hello ");
+	System.out.println("Variete Kesara");
+	System.out.println("Age 15 years");
+	}
+}
+

1445a3b8e8656051fbbd98748878f5268434fab5
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-10-06 22:03:06 +0530

    import

---
+++ FarmingJava/src/com/xworkz/FarmingJava/Vegitables/DrumStick.java
@@ -0,0 +1,11 @@
+package com.xworkz.FarmingJava.Vegitables;
+
+public class DrumStick {
+	static void main3 () {
+
+	
+		System.out.println("Vegitable name Drum stick");
+		System.out.println("Age 5 years");
+
+	}
+}
---
+++ FarmingJava/src/com/xworkz/FarmingJava/Vegitables/Pomogranate.java
@@ -0,0 +1,11 @@
+package com.xworkz.FarmingJava.Vegitables;
+
+public class Pomogranate {
+	static void main1 () {
+
+		System.out.println("Name Rudreshwar");
+		System.out.println("Experiance 3 Years");
+
+	}
+}
+

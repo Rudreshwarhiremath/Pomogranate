ec362aadc23497916f25bbeb8388f97cb5540684
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-10-09 21:57:54 +0530

    7 0ct import

---
+++ FarmingJava/src/com/xworkz/FarmingJava/boost/Num.java
@@ -0,0 +1,17 @@
+package com.xworkz.FarmingJava.boost;
+
+public class Num {
+	
+	public static void multiplication2() {
+		for(int j=1;j<10;j++) {
+			int n=2;
+			System.out.println(j*n);
+		}
+	}
+   public static void multiplication3() {
+	   for(int k=1;k<10;k++) {
+		   int m=3;
+		   System.out.println(k*m);
+	   }
+   }
+}

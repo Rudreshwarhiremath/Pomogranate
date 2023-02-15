5d52c618ff6f4b0093f3ff5f4063cb4584440d6a
rg hiremath <rudreshwar.xworkz@gmail.com>
2023-02-15 21:58:05 +0530

    swim

---
+++ swimming/src/main/java/com/xworkz/swimming/dto/Cardto.java
@@ -0,0 +1,24 @@
+package com.xworkz.swimming.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="car")
+public class Cardto {
+	@Column(name="id")
+	@Id
+	private Integer id;
+	@Column(name="car_name")
+	private String name;
+	@Column(name="car_brand")
+	private String brand;
+	@Column(name="car_rate")
+	private String rate;
+
+}
